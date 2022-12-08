1b55f8a48b2961b6153b13fd5d1afaef04e61856
zyzzchad <113049704+zyzzchad@users.noreply.github.com>
2022-12-08 13:24:07 +0545

    css changes

--- Css/new.java
+++
@@ -1,9 +0,0 @@
-public class age{
-    public static void main(String[] args){
-        int age=18;
-        if(age>16){
-            System.out.print("Age is greater than 16");
-
-        }
-    }
-}
